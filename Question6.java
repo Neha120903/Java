a121c993bec514dd259cd9e87847a86cff2360ed
Neha Sharma <86730309+Neha120903@users.noreply.github.com>
2021-10-07 16:11:13 +0530

    Add files via upload

---
+++ Question6.java
@@ -0,0 +1,10 @@
+/* Write a Program to accept two integers as command line arguments and print the sum of the two numbers
+Example1)
+C:\>java filename 10 20
+O/P Expected : The sum of 10 and 20 is 30*/
+package Assingment1;
+public class Question6 {
+	public static void main(String [] args) {
+	System.out.println("The sum of "+args[0]+" and "+args[1]+" is "+args[0]+args[1]);
+	}
+}
