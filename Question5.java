98b23ae7b30d89616ab51f27dff2c31dc8953d79
Neha Sharma <86730309+Neha120903@users.noreply.github.com>
2021-10-07 16:10:50 +0530

    Add files via upload

---
+++ Question5.java
@@ -0,0 +1,11 @@
+/*Write a Program to accept a String as a command line argument and print a Welcome message as given below.
+Example1)
+C:\> java Sample Aman
+O/P Expected : Welcome Aman*/
+package Assingment1;
+public class Question5 {
+	public static void main(String[] args) {
+		System.out.println("Welcome "+args[2]);
+	}
+
+}
