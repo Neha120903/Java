19d0500416f013a2661a64b464313a4fcbddd250
Neha Sharma <86730309+Neha120903@users.noreply.github.com>
2021-11-26 06:30:26 +0530

    Update t3.java

--- t3.java
+++ t3.java
@@ -1,3 +1,6 @@
+/*Create two threads, one thread to display all even numbers between 1 & 20, another to display odd numbers between 1 & 20.
+Note: Display all even numbers followed by odd numbers
+Hint: use join.     (use of join method and thread control mechanism)*/
 package Thread;
 
 public class t3 {
