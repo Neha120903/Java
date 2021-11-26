38e0d5aea5d7a3ba4c0b0de49b75e7df9515c94f
Neha Sharma <86730309+Neha120903@users.noreply.github.com>
2021-11-26 06:17:27 +0530

    Add files via upload

---
+++ t1.java
@@ -0,0 +1,12 @@
+package Thread;
+public class t1 {
+	public static void main(String[] args)
+	{
+		Thread t1 = new Thread();
+		t1.setName("Scooby");
+		System.out.println(t1.getName());
+		Thread t2 = new Thread();
+		t2.setName("Shaggy");
+		System.out.println(t2.getName());
+	}
+}
