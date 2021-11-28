8faf51f414026c455a296d8ef1c003353396ee7f
Neha Sharma <86730309+Neha120903@users.noreply.github.com>
2021-11-28 10:38:32 +0530

    Add files via upload

---
+++ ScannerClass.java
@@ -0,0 +1,22 @@
+package hackerrank;
+import java.util.Scanner;
+
+public class ScannerClass {
+	 public static void main(String[] args) {
+	        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. 
+	        Scanner line = new Scanner(System.in);
+	        int counter =1;
+	        while(line.hasNextLine()){
+	            String s = line.nextLine();
+	            System.out.println(counter+" "+s);
+	            counter++;*/
+		 Scanner sc = new Scanner(System.in);
+		 int i= sc.nextInt();
+		 float f = sc.nextFloat();
+		 sc.nextLine();
+		 String s = sc.nextLine();
+		 System.out.println(s);
+		 System.out.println(f);
+		 System.out.println(i);
+	        }
+	    }
