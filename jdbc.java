327651cf11ea93f7e6361208da52e1a0650a112f
Neha Sharma <86730309+Neha120903@users.noreply.github.com>
2021-12-04 18:20:19 +0530

    with query

--- jdbc.java
+++ jdbc.java
@@ -8,13 +8,13 @@ public class jdbc {
 		{
 		try 
 		{	
-		String query ="select username form student where user_id=2";
+		String query ="select * from student";
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/new","root","");
 		Statement st = con.createStatement();
 		ResultSet rs =st.executeQuery(query);
 		rs.next();
-		String name = rs.getString(0);
+		String name = rs.getString("username");
 		System.out.println(name);
 		st.close();
 		con.close();
